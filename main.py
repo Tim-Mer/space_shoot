863f277e89f0cfb17953e48a41ca51dfcef2d287
Tim-Mer <timmymerriman@gmail.com>
2025-04-01 14:15:21 -0700

    Adding clock

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def main():
     pygame.init()
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     
     
     while True:
@@ -16,7 +18,8 @@ def main():
                 return
         pygame.Surface.fill(screen, (0,0,0))        
         pygame.display.flip()
-
+        
+        dt = clock.tick(60)/1000
 
 
 
