ecbeb5631998786e4d6d85a2885ebd3a4499b538
Tim-Mer <timmymerriman@gmail.com>
2025-03-31 19:53:20 -0700

    Starting point

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # space_shoot
+
+| Setup
+source setup.sh
+
+
+boot.dev asteroids project
---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame
+
+def main():
+    print("Starting Asteroids!")
+    
+    
+    
+
+
+
+
+
+if __name__ == "__main__":
+    main()
