850fa3015b853cecb295056cf16680f63cc65e65
Tim-Mer <timmymerriman@gmail.com>
2025-04-01 15:12:24 -0700

    Asteroids not spawning

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pygame
 from constants import *
+from player import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
     print("Starting Asteroids!")
@@ -11,15 +14,35 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    asteroidfield = AsteroidField()
+    
+    #GAME LOOP
     while True:
+        #START
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (0,0,0))        
-        pygame.display.flip()
+        pygame.Surface.fill(screen, (0,0,0))
+             
+        #MIDDLE
+        updatable.update(dt)
         
+        
+        #END
+        for item in drawable:
+            item.draw(screen)
+         
         dt = clock.tick(60)/1000
+        pygame.display.flip()
 
 
 
